2e7585d26fd9a082b4cc8f8037cfcf3b2e88374a
Charles Butler <cdbutle@outlook.com>
2020-04-11 21:24:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,6 +25,7 @@ class Student
     DROP TABLE IF EXISTS students
     SQL
     DB[:conn].execute.(sql)
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
